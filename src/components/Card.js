dca39f2c95c044310dc64e87f6ccc305adb5f6c1
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-16 20:54:26 +0000

    Adding card heading

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,11 @@ import React from "react";
 const Card = () => {
   return (
     <div className="card">
-      <div className="card__title">MeetYourMentor</div>
+      <div className="card__heading">
+        <div className="card__icon"></div>
+        <div className="card__title">MeetYourMentor</div>
+      </div>
+
       <div className="card__body"></div>
     </div>
   );
