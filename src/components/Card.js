9a1396075234f073b7d0eab5974e4d058c0bf4a2
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-16 10:16:07 +0000

    Added card component

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="card__title">MeetYourBuddy</div>
+      <div className="card__body"></div>
+    </div>
+  );
+};
+
+export default Card;
