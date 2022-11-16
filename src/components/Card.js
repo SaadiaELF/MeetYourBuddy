e276122acb17954a98b3970d712b6be239430477
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-16 20:15:11 +0000

    Chnaged title

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
   return (
     <div className="card">
-      <div className="card__title">MeetYourBuddy</div>
+      <div className="card__title">MeetYourMentor</div>
       <div className="card__body"></div>
     </div>
   );
