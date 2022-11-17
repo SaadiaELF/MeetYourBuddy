0abfb12c2ea633d5ca4bceef0299271040513895
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-17 16:45:00 +0000

    Updated styles

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -3,9 +3,11 @@ import React from "react";
 const Dropdown = (props) => {
   return (
     <div className="dropdown">
-      <label htmlFor={props.name}>Select a {props.option}:</label>
+      <label className="dropdown-label" htmlFor={props.name}>
+        Select a {props.option}
+      </label>
 
-      <select name={props.name} id={props.name}>
+      <select className="dropdown-select" name={props.name} id={props.name}>
         <option key="-1" value="-1">
           Select a {props.option}
         </option>
