17071494d34c8acc076c101eee7d1c6f5dae4443
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-18 10:54:57 +0000

    Adding new Calendar npm

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,29 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "../Dropdown/Dropdown";
 import mentors from "../../data/mentors";
 import Button from "../Button/Button";
-import "react-calendar/dist/Calendar.css";
+import { Calendar } from "primereact/calendar";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 const Card = () => {
   const technologies = ["HTML", "CSS", "Javascript", "React"];
-  // const [date, setDate] = useState(new Date());
-  // const [selected, setSelected] = useState("");
+  const [dates, setDates] = useState([new Date()]);
 
-  // const handleMentorsDropdown = (e) => {
-  //   console.log("test", e.target.value);
-  //   let mentor = mentors.filter((mentor) => mentor.id === e.target.value);
-  //   let dates = Object.keys(mentor[0].availability);
-  //   let datesArr = dates.map((date) => {
-  //     let arr = date.split("/");
-  //     return (date = new Date(
-  //       parseInt(arr[2]),
-  //       parseInt(arr[0]) - 1,
-  //       parseInt(arr[1])
-  //     ));
-  //   });
-  //   console.log(datesArr, new Date());
-  //   setDate(new Date(2022, 10, 19), new Date(2022, 10, 22));
-  // };
+  const handleMentorsDropdown = (e) => {
+    let mentor = mentors.filter((mentor) => mentor.id === e.target.value);
+    let dates = Object.keys(mentor[0].availability);
+    dates = dates.map((date) => {
+      let arr = date.split("/");
+      return (date = new Date(arr[2], arr[0] - 1, arr[1]));
+    });
+    setDates(dates);
+  };
+
   return (
     <div className="card">
       <div className="card__heading">
@@ -38,12 +35,17 @@ const Card = () => {
           options={technologies}
         />
         <Dropdown
-          // onChange={handleMentorsDropdown}
+          onChange={handleMentorsDropdown}
           name="mentors"
           option="mentor"
           options={mentors}
         />
-        {/* <Calendar onChange={setDate} value={date} /> */}
+        <Calendar
+          selectionMode="multiple"
+          inline
+          onChange={setDates}
+          value={dates}
+        />
         <Button />
       </div>
     </div>
